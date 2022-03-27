987432a7add084b2eca1b52683641798b15622e0
Sergey Leschev <sergey.leschev@gmail.com>
2022-03-27 15:46:50 +0300

    2. Add Two Numbers

--- 0002. Add Two Numbers.ts
+++ 0002. Add Two Numbers.ts
@@ -41,22 +41,28 @@
  * }
  */
 
-function addTwoNumbers(
-  l1: ListNode | null,
-  l2: ListNode | null,
-  carry = 0
-): ListNode | null {
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null, carry = 0): ListNode | null {
   if (l1 || l2) {
     const next1 = getNextNode(l1)
     const next2 = getNextNode(l2)
     const sum = getNodeValue(l1) + getNodeValue(l2) + carry
     const nextCarry = sum >= 10 ? 1 : 0
-
+    
     return new ListNode(sum % 10, addTwoNumbers(next1, next2, nextCarry))
-  } else if (carry) {
-    return new ListNode(1)
-  }
-
+  } else if (carry) { return new ListNode(1) }
+    
   return null
 }
 
