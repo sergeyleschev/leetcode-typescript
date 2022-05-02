c43a59e9d9888adad5b1fc77c240a8116b03fe43
Sergey Leschev <sergey.leschev@gmail.com>
2022-05-02 13:17:19 +0300

    441. Arranging Coins

---
+++ 0441. Arranging Coins.ts
@@ -0,0 +1,34 @@
+// Solution @ Sergey Leschev, Belarusian State University
+
+// 441. Arranging Coins
+// You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
+// Given the integer n, return the number of complete rows of the staircase you will build.
+
+// Finds the total number of full staircase rows that can be formed from n coins.
+
+// - Parameter n: The number of coins.
+// - Returns: The number of full staircase.
+
+// Example 1:
+// Input: n = 5
+// Output: 2
+// Explanation: Because the 3rd row is incomplete, we return 2.
+
+// Example 2:
+// Input: n = 8
+// Output: 3
+// Explanation: Because the 4th row is incomplete, we return 3.
+
+// Constraints:
+// 1 <= n <= 2^31 - 1
+
+function arrangeCoins(n: number): number {
+  let i = 0
+
+  while (i < n) {
+    i += 1
+    n -= i
+  }
+
+  return i
+}
