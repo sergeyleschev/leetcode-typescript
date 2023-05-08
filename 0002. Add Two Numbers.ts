7642b61755e1e3b0794bdc3db90c591a7c684d35
Sergey Leschev <sergey.leschev@gmail.com>
2023-05-08 23:14:02 +0300

    2. Add Two Numbers

--- 0002. Add Two Numbers.ts
+++ 0002. Add Two Numbers.ts
@@ -1,4 +1,4 @@
-// Solution @ Sergey Leschev, Belarusian State University
+// Solution @ Sergey Leschev
 
 // 2. Add Two Numbers
 // You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
